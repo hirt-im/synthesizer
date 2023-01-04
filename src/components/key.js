dbd3cf3cfd26edf1dee56fe5df62d6dd14dc06b3
Michael Hirt <hirtm14@gmail.com>
2023-01-04 12:29:42 -0500

    Fix layout of keys, add placeholder functions for clicking keys

--- src/components/key.js
+++ src/components/key.js
@@ -1,12 +1,24 @@
 export default function Key(props){
-    let className;
-    if (props.note[1] === "#"){
-        className = "key sharp";
+    let sharpNote = props.note + "#";
+
+    const playNote = () => {
+        console.log(props.note);
     }
-    else{
-        className= "key natural";
+
+    const playNoteSharp = (e) => {
+        console.log(sharpNote)
+        e.stopPropagation();
     }
+
+    if (props.note === 'E' || props.note === 'B' || props.sharp === 'no'){
+        return (
+            <div className={'key natural ' + props.note} onClick={playNote}></div>
+        );
+    }
+
     return (
-        <div className={className}>{props.note}</div>
+        <div className={'key natural ' + props.note} onClick={playNote}>
+            <div className={'key sharp ' + sharpNote} onClick={playNoteSharp}></div>
+        </div>
     );
 }
--- src/components/keyboard.js
+++ src/components/keyboard.js
@@ -4,16 +4,13 @@ export default function Keyboard(){
     return(
         <div className='keyboard'>
             <Key note="C"></Key>
-            <Key note="C#"></Key>
             <Key note="D"></Key>
-            <Key note="D#"></Key>
             <Key note="E"></Key>
             <Key note="F"></Key>
             <Key note="G"></Key>
-            <Key note="G#"></Key>
             <Key note="A"></Key>
             <Key note="B"></Key>
-            <Key note="C"></Key>
+            <Key note="C" sharp='no'></Key>    
         </div>
     );
 }
