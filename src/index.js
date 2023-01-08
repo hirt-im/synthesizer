f22f239257c0558b717090fad108ab1fdb909af5
Michael Hirt <hirtm14@gmail.com>
2023-01-08 12:01:44 -0500

    Can now play 2 octaves on the keyboard

--- src/index.js
+++ src/index.js
@@ -17,53 +17,99 @@ root.render(
 
 let octave = 4;
 let keypresses = {
-  'a': 'C',
-  'w': 'C#',
-  's': 'D',
-  'e': 'D#',
-  'd': 'E',
-  'f': 'F',
-  't': 'F#',
-  'g': 'G',
-  'y': 'G#',
-  'h': 'A',
-  'u': 'A#',
-  'j': 'B',
-  'k': 'C'
+  'z': 'C',
+  's': 'C#',
+  'x': 'D',
+  'd': 'D#',
+  'c': 'E',
+  'v': 'F',
+  'g': 'F#',
+  'b': 'G',
+  'h': 'G#',
+  'n': 'A',
+  'j': 'A#',
+  'm': 'B',
+  ',': 'C'
+}
+
+let keypresses2 = {
+  'q': 'C',
+  '2': 'C#',
+  'w': 'D',
+  '3': 'D#',
+  'e': 'E',
+  'r': 'F',
+  '5': 'F#',
+  't': 'G',
+  '6': 'G#',
+  'y': 'A',
+  '7': 'A#',
+  'u': 'B',
+  'i': 'C'
 }
 
 let currNotes = {};
 
 document.addEventListener('keydown', (e) => {
-  if (e.key === 'x'){
+
+  if (e.key === '/'){
     octave++;
+    return;
   }
-  if (e.key === 'z'){
+
+  if (e.key === '.'){
     octave--;
+    return;
   }
+
   if (keypresses[e.key]){
     if (!currNotes[e.key]){
-      if (e.key === 'k'){
+      if (e.key === ','){
         synth.triggerAttack(keypresses[e.key] + (octave + 1).toString(), Tone.now(), effectValues["Velocity"]);
-        // synth.triggerAttack(keypresses[e.key] + (octave + 1).toString(), Tone.now());
       }
-      else {
+      else{
         synth.triggerAttack(keypresses[e.key] + octave.toString(), Tone.now(), effectValues["Velocity"]);
-        // synth.triggerAttack(keypresses[e.key] + octave.toString(), Tone.now());
       }
       currNotes[e.key] = true;
     }
+    return;
+  }
+
+  if (keypresses2[e.key]){
+    octave++;
+    if (!currNotes[e.key]){
+      if (e.key === 'i'){
+        synth.triggerAttack(keypresses2[e.key] + (octave + 1).toString(), Tone.now(), effectValues["Velocity"]);
+      }
+      else {
+        synth.triggerAttack(keypresses2[e.key] + octave.toString(), Tone.now(), effectValues["Velocity"]);
+      }
+      currNotes[e.key] = true;
+    }
+    octave--;
   }
 })
+
 
 document.addEventListener('keyup', (e) => {
-  if (e.key === 'k'){
-    synth.triggerRelease(keypresses[e.key] + (octave + 1).toString());
+
+  if(keypresses[e.key]){
+    if (e.key === ','){
+      synth.triggerRelease(keypresses[e.key] + (octave + 1).toString());
+    }
+    synth.triggerRelease(keypresses[e.key] + octave.toString());
   }
-  synth.triggerRelease(keypresses[e.key] + octave.toString());
+
+  if(keypresses2[e.key]){
+    octave++;
+    if (e.key === 'i'){
+      synth.triggerRelease(keypresses2[e.key] + (octave + 1).toString());
+    }
+    synth.triggerRelease(keypresses2[e.key] + octave.toString());
+    octave--;
+  }
   delete currNotes[e.key];
 })
-
 
 
 export { octave };
